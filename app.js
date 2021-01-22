21b84334d2d54dafdb4cd0330daa11b00eee38a7
BrenoProti <moreirasgbr@gmail.com>
2021-01-22 01:40:36 -0300

    uptade app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var submitButton = document.querySelector('#app form button');
 var zipCodeField = document.querySelector('#app form input');
-var content = document.querySelector('#app maim');
+var content = document.querySelector('#app main');
 
 submitButton.addEventListener('click', run);
 
@@ -13,4 +13,24 @@ function run(event) {
     zipCode = zipCode.trim(); //Remove spaces
 
     console.log(zipCode)
+
+    axios.get(`https://viacep.com.br/ws/${zipCode}/json/`).then(function(response) {
+        console.log(response.data);
+        content.innerHTML='';
+        createLine(response.data.logradouro);
+        createLine(response.data.complemento);
+        createLine(response.data.bairro);
+        createLine(response.data.localidade + ' - ' + response.data.uf);
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+}
+
+function createLine(text) {
+        var line = document.createElement('p');
+        var text = document.createTextNode(text)
+
+        line.appendChild(text)
+        content.appendChild(line)
 }
